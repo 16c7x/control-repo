a661e2a3ad057144ed8a88b2970e79ae23efb4ac
Andrew Jones <pic16c7x@gmail.com>
2020-01-04 16:58:20 +0000

    Update puppetmaster.pp

--- site-modules/profile/manifests/puppetmaster.pp
+++ site-modules/profile/manifests/puppetmaster.pp
@@ -1,3 +1,23 @@
 class profile::puppetmaster {
-  notify { "Puppet master profile": }
+  notify { "Conf Dir : ${confdir}": }
+  ini_setting { 'policy-based autosigning':
+    setting => 'autosign',
+    path    => "${confdir}/puppet.conf",
+    section => 'master',
+    value   => '/opt/puppetlabs/puppet/bin/autosign-validator',
+    notify  => Service['pe-puppetserver'],
+  }
+
+  class { ::autosign:
+    ensure => 'latest',
+    config => {
+      'general' => {
+        'loglevel' => 'INFO',
+      },
+      'jwt_token' => {
+        'secret'   => 'hunter2'
+        'validity' => '7200',
+      }
+    },
+  }
 }
