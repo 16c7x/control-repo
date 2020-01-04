661718952c05c0017736d6e3b1441bf1add24618
Andrew Jones <pic16c7x@gmail.com>
2020-01-04 16:27:13 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -32,25 +32,7 @@ node default {
 
 node 'puppetmaster.lab' {
   notify { "Puppet Master": }
-
-  ini_setting { 'policy-based autosigning':
-    setting => 'autosign',
-    path    => "/etc/puppetlabs/puppet/puppet.conf",
-    section => 'master',
-    value   => '/opt/puppetlabs/puppet/bin/autosign-validator',
-    notify  => Service['pe-puppetserver'],
-  }
-
-  class { ::autosign:
-    ensure => 'latest',
-    config => {
-      'general' => {
-        'loglevel' => 'INFO',
-      },
-      'jwt_token' => {
-        'secret'   => 'hunter2'
-        #'validity' => '7200',
-      }
-    },
-  }
+node 'puppetmaster.lab' {
+  notify { "Puppet Master": }
+  include ::profile::puppetmaster
 }
