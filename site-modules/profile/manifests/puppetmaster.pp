c4d8663e82ae48f1dbcbc393cbea5f2a5c6f9bc1
Andrew Jones <pic16c7x@gmail.com>
2020-02-13 13:10:33 +0000

    Update puppetmaster.pp

--- site-modules/profile/manifests/puppetmaster.pp
+++ site-modules/profile/manifests/puppetmaster.pp
@@ -3,6 +3,16 @@ class profile::puppetmaster {
 
   notify { 'Puppet Master Profile': }
 
+  node_group { 'PE MCollective':
+    ensure               => 'present',
+    #classes              => {'puppet_enterprise::profile::mcollective::agent' => {}},
+    environment          => 'production',
+    #id                   => '4cdec347-20c6-46d7-9658-7189c1537ae9',
+    override_environment => 'false',
+    parent               => 'PE Master',
+    rule                 => ['and', ['~', ['fact', 'function'], 'compiler']],
+  }
+
   ini_setting { 'policy-based autosigning':
     setting => 'autosign',
     path    => "${settings::confdir}/puppet.conf",
