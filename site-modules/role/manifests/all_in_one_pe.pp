c78088312108998e28caaf23d01c61a613a22ef3
andrew <andrew.jones@puppet.com>
2020-04-23 16:48:47 +0100

    all in one pe

--- site-modules/role/manifests/all_in_one_pe.pp
+++ site-modules/role/manifests/all_in_one_pe.pp
@@ -1,5 +1,7 @@
 #
-include ::profile::master::nodegroups
-include ::profile::master::puppetmaster
-include ::profile::base
-notify { 'All in one pe': }
+class role::all_in_one_pe {
+  include ::profile::master::nodegroups
+  include ::profile::master::puppetmaster
+  include ::profile::base
+  notify { 'All in one pe': }
+}
