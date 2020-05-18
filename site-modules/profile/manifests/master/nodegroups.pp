8e98d301ea9da28204f1c91592d1e33eb202bbc4
Andrew Jones <pic16c7x@gmail.com>
2020-05-18 14:50:46 +0100

    Update nodegroups.pp

--- site-modules/profile/manifests/master/nodegroups.pp
+++ site-modules/profile/manifests/master/nodegroups.pp
@@ -3,12 +3,20 @@ class profile::master::nodegroups {
   node_group { 'PE Master':
     rule                 => ['and', ['=', ['fact', 'function'], 'compiler']],
   }
+  node_group { 'PE Controler':
+    ensure               => 'present',
+    classes              => {'puppet_enterprise::profile::controller' => {}},
+    environment          => 'production',
+    override_environment => false,
+    parent               => 'All Nodes',
+    rule                 => ['and', ['=', ['fact', 'function'], 'controler']],
+  }  
   node_group { 'role::test_application':
     ensure               => 'present',
     classes              => {'role::test_application' => {}},
     environment          => 'production',
     override_environment => false,
     parent               => 'All Nodes',
-    rule                 => ['and', ['=', ['fact', 'function'], 'controler']],
+    rule                 => ['and', ['=', ['fact', 'function'], 'testapp']],
   }
 }
