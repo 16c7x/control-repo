0e1f7ad874d24262491c264058cbf153b95b6a12
andrew <andrew.jones@puppet.com>
2020-05-27 15:28:01 +0100

    wip

--- site-modules/profile/manifests/baseline/windows.pp
+++ site-modules/profile/manifests/baseline/windows.pp
@@ -73,16 +73,16 @@ class profile::baseline::windows (
   }
 
 # Enable IE ESC
-  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}':
+  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
     ensure => present,
-    value  => 'IsInstalled',
+    #value  => 'IsInstalled',
     type   => dword,
     data   => '1',
   }
 
-  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}':
+  registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
     ensure => present,
-    value  => 'IsInstalled',
+    #value  => 'IsInstalled',
     type   => dword,
     data   => '1',
   }
