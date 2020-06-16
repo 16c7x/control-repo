4f61f29f92d9baa4127cb30dba46321c07747f60
Andrew Jones <pic16c7x@gmail.com>
2020-06-16 21:44:57 +0100

    Update all_in_one_pe.pp

--- site-modules/role/manifests/all_in_one_pe.pp
+++ site-modules/role/manifests/all_in_one_pe.pp
@@ -2,5 +2,5 @@
 class role::all_in_one_pe {
   #include ::profile::master::nodegroups
   #include ::profile::master::puppetmaster
-  #include ::profile::baseline::baseline
+  include ::profile::baseline::baseline
 }
