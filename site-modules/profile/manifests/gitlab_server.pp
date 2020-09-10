54d9cd87b03ce908774b79e25c6135d02c4f87ed
Andrew Jones <pic16c7x@gmail.com>
2020-09-10 19:10:57 +0100

    Update gitlab_server.pp

--- site-modules/profile/manifests/gitlab_server.pp
+++ site-modules/profile/manifests/gitlab_server.pp
@@ -3,7 +3,7 @@ class profile::gitlab_server {
   class { 'gitlab':
     external_url => 'http://gitlab-dev.platform9.puppet.net',
     gitlab_rails => {
-      'omniauth_allow_single_sign_on' => 'saml',
+      'omniauth_allow_single_sign_on' => ['saml'],
       'omniauth_block_auto_created_users' => false,
     },
   }
