0fcd9edbb33c455f2351e2d41ac522ff81f0f884
Andrew Jones <pic16c7x@gmail.com>
2020-09-22 08:41:39 +0100

    Update cis.pp

--- site-modules/profile/manifests/cis.pp
+++ site-modules/profile/manifests/cis.pp
@@ -1,4 +1,8 @@
 # CIS
 class profile::cis {
-  class { '::secure_linux_cis': }
+class {'::secure_linux_cis':
+   time_servers  => ['time1.google.com', 'time1.google.com'],
+   profile_type  => 'centos',
+   allow_users   => 'trusteduser',
+}
 }
