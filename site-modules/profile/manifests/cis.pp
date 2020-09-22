06fa82ca20350f3d0b8d96ec804e5a3e5debce56
Andrew Jones <pic16c7x@gmail.com>
2020-09-22 09:34:36 +0100

    Update cis.pp

--- site-modules/profile/manifests/cis.pp
+++ site-modules/profile/manifests/cis.pp
@@ -1,8 +1,9 @@
 # CIS
 class profile::cis {
-  class {'::secure_linux_cis':
-    time_servers  => ['time1.google.com', 'time1.google.com'],
-    profile_type  => 'server',
-    allow_users   => ['centos'],
-  }
+  #class {'::secure_linux_cis':
+  #  time_servers  => ['time1.google.com', 'time1.google.com'],
+  #  profile_type  => 'server',
+  #  allow_users   => ['centos'],
+  #}
+  include secure_linux_cis::rules::ensure_separate_partition_exists_for_var_log
 }
