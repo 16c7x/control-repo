2116369a9d15b20eb3625af302df42da7b4e679b
Andrew Jones <pic16c7x@gmail.com>
2020-09-26 16:43:39 +0100

    Create puppet_proxy.pp

---
+++ site-modules/profile/manifests/puppet_proxy.pp
@@ -0,0 +1,23 @@
+#
+class profile::puppet_proxy{
+  include ::haproxy
+  haproxy::listen { 'proxy1':
+    collect_exported => false,
+    ipaddress        => $::ipaddress,
+    ports            => '8140',
+  }
+  haproxy::balancermember { 'compiler3':
+    listening_service => 'puppet00',
+    server_names      => 'compiler3.platform9.puppet.net',
+    ipaddresses       => '10.234.5.134',
+    ports             => '8140',
+    options           => 'check',
+  }
+  haproxy::balancermember { 'compiler4':
+    listening_service => 'puppet00',
+    server_names      => 'compiler4.platform9.puppet.net',
+    ipaddresses       => '10.234.5.149',
+    ports             => '8140',
+    options           => 'check',
+  }
+}
