568be67d16c1586817fdb8149d1e3da78df7bbec
Andrew Jones <pic16c7x@gmail.com>
2020-09-27 19:24:42 +0100

    Update puppet_proxy.pp

--- site-modules/profile/manifests/puppet_proxy.pp
+++ site-modules/profile/manifests/puppet_proxy.pp
@@ -6,18 +6,18 @@ class profile::puppet_proxy{
     ipaddress        => $::ipaddress,
     ports            => '8140',
   }
-  haproxy::balancermember { 'compiler1':
-    listening_service => 'puppet00',
-    server_names      => 'compiler1.platform9.puppet.net',
-    ipaddresses       => '192.168.0.6',
-    ports             => '8140',
-    options           => 'check',
-  }
+  #haproxy::balancermember { 'compiler1':
+  #  listening_service => 'puppet00',
+  #  server_names      => 'compiler1.platform9.puppet.net',
+  #  ipaddresses       => '192.168.0.6',
+  #  ports             => '8140',
+  #  options           => 'check',
+  #}
   haproxy::balancermember { 'compiler2':
     listening_service => 'puppet00',
     server_names      => 'compiler2.platform9.puppet.net',
     ipaddresses       => '192.168.0.27',
-    ports             => '8140',
+    ports             => '22',
     options           => 'check',
   }
 }
